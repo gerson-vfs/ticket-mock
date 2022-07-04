0c1cb3e27f80425a859a898161a9e9db4dff8daf
João Neto Castro <jneto111@gmail.com>
2022-07-04 08:29:04 -0300

    feature: authenticate route

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,12 @@
 const jwt = require('jsonwebtoken')
 
+const jwtSecret = 'aaw1vmv0e93no3F8jw1d10C'
 export const generateMockedAuthentication = () => {
+    console.log('generating token')
     const token = {
-        username: 'any_username'
+        username: 'any_username@mail.com'
     }
-    const id_token = jwt.create(token)
+    const id_token = jwt.sign(token, jwtSecret)
     return {
         id_token,
         access_token: 'any_id',
